2f1cf81e49cc5d45295783e527b2d868fc5f13d1
Haozhen Wu <wuhaozhen1106@gmail.com>
2018-03-27 14:32:02 +0800

    small change in gui class name

--- src/gui/PseudocodeInterpreter.java
+++ src/gui/PseudocodeInterpreter.java
@@ -3,42 +3,39 @@ package gui;
 import core.interpreter.Interpreter;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
 
-public class GuiName extends JFrame {
+public class PseudocodeInterpreter extends JFrame {
 
     private JTextPane programTextPane;
     private JTextPane outputTextPane;
     private JButton runButton;
     private JPanel jPanel;
 
-    public GuiName()
-    {
-        super("GuiName");
+    public PseudocodeInterpreter() {
+        super("PseudocodeInterpreter");
         setContentPane(jPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        runButton.addActionListener(this::ButtonRunClick);
+        runButton.addActionListener(e -> buttonRunClick());
 
-        Interpreter.addOutputListener(this::ProgramText);
+        Interpreter.addOutputListener(this::programText);
 
         pack();
     }
 
-    private void ButtonRunClick(ActionEvent arg)
-    {
+    private void buttonRunClick() {
         String text = programTextPane.getText();
+        outputTextPane.setText("");
         Interpreter.interpret(text);
         Interpreter.execute();
     }
 
-    private void ProgramText(String arg)
-    {
+    private void programText(String text) {
         String originalText = outputTextPane.getText();
-        outputTextPane.setText(originalText + arg + "\n");
+        outputTextPane.setText(originalText + text + "\n");
     }
 
     public static void main(String[] args) {
-        new GuiName().setVisible(true);
+        new PseudocodeInterpreter().setVisible(true);
     }
 }
